89ca7fb46787982f1b3b94618fb77eb19ced8c76
iamkentleom <leomakibulan@gmail.com>
2019-08-10 12:19:06 +0800

    map still not working

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
+// import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 import './index.css';
+
+
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 const mapStyles = {
   width: '100%',
   height: '100%'
 };
 
-export class MapContainer extends React.Component {
+class MapContainer extends React.Component {
   render() {
     return (
       <Map
@@ -28,7 +31,56 @@ export default GoogleApiWrapper({
   apiKey: 'AIzaSyClD0yfdkOWAkjroWHOdfSqUYa4_N5B47Q'
 })(MapContainer);
 
+
+class MapContain extends React.Component {
+  render(){
+    return (
+      <div className="map-size">
+      </div>
+    )
+  }
+}
+
+class Details extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render(){
+    return (
+      <div className={this.state.isToggleOn ? 'details' : 'details-full'} onClick={this.handleClick}>
+        <h2>Fire in Hogwarts</h2>
+        Location:<br/>
+        Latitude:<br/>
+        Longitude:<br/>
+        Reporters:<br/>
+        Details:<br/>
+      </div>
+    )
+  }
+}
+
+function App(){
+  return (
+    <div>
+
+      <MapContainer />
+      <Details />
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <MapContainer/>,
+  <App/>,
   document.getElementById('root')
 );
