ebbb17b49837fb4e699c4473b412b17806684a6e
iamkentleom <leomakibulan@gmail.com>
2019-08-10 11:25:04 +0800

    map

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 import './index.css';
 
-const title = <h1>More Say</h1>
-  
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+export class MapContainer extends React.Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+         lat: -1.2884,
+         lng: 36.8233
+        }}
+      />
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyClD0yfdkOWAkjroWHOdfSqUYa4_N5B47Q'
+})(MapContainer);
+
 ReactDOM.render(
-  title,
+  <MapContainer/>,
   document.getElementById('root')
 );
