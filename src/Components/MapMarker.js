c4c18b5cb425b348ad5b23f112a7384b54014e60
James <wayperwayp@gmail.com>
2019-08-10 13:26:00 +0800

    added 2nd screen

--- src/Components/MapMarker.js
+++ src/Components/MapMarker.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 export class MapMarker extends Component {
+    
     render() {
         return <div>Marker stupid</div>;
     }
---
+++ src/Components/ReportScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class ReportScreen extends Component {
+    render() {
+        return (
+            <div>
+                2nd Screen
+                
+            </div>
+        )
+    }
+}
+
+export default ReportScreen
